273a2e357ba2b18507dec8906c7d358cdca5f10f
Dan Serban <dserban01@gmail.com>
2012-06-16 13:09:36 +0300

    With initial hardcoded list of habits to kickstart the project into high gear

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
 var weekDays=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+var habits=new Array();
+habits.push("Drink at least 10 glasses of water every day");
+habits.push("Eat a healthy diet");
+habits.push("Spend at least 15 minutes a day learning a new technology");
+habits.push("Be punctual");
+habits.push("Brisk-walk at least 30 minutes a day");
+habits.push("Brush teeth twice a day");
+habits.push("Help a friend");
+habits.push("Refrain from watching TV");
 
