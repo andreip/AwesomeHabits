d3240439ed007ef3980a161c9937b02a47c2e43e
Constantin Miroslav <costin.miroslav@gmail.com>
2012-06-16 17:30:22 +0300

    added TrackProgressView use .visualstate

--- js/app_progress.js
+++ js/app_progress.js
@@ -4,45 +4,71 @@ $(function(){
 	var TrackModel = Backbone.Model.extend({
 			initialize: function() {
 			if (!this.get("name")){
-				this.set({"name":"habbit 1"}); 
+				this.set({"name":"habit"}); 
 				this.set({"progress":32});
 				}
 			}
 	});
 
-
-
-  var TrackProgressView = Backbone.View.extend({
+  var TrackProgressEntryView = Backbone.View.extend({
     tagName:  "li",
     className: "hero-entry",
     template: _.template($('#progress-template').html()),
     events: {},
     initialize: function() {
     },
+
     render: function() {
       this.$el.html(this.template(this.model.toJSON()));
       return this;
     }
   });
-	$('#progress-link').bind('click', function() {
-    $('#eachday').hide();
-    $('#welcomescreen').hide();
-    
-		$('#trackprogress').remove();
-		$('div.hero-unit').append('<div id="trackprogress"></div> <!-- track progress -->')
-		var allData = window.dailyHabits
-		var habbits = window.habits    
-		for (var i = 0;i<habbits.length;i++){
-      var progressData = _.map(allData, 
-        function(dataForADay){return dataForADay[i];
-        });
-      var progressSum = _.reduce(progressData, 
-        function(memo, num){return memo + num;
-        });
-			var tm = new TrackModel({name: habbits[i], progress:(progressSum*100/7).toFixed(2)});
-  		var view = new TrackProgressView({model: tm});
-  		$('#trackprogress').append(view.render().el)
-  	}
 
-  });//end progress-link bind click
+  var TrackProgressView = Backbone.View.extend({
+    el: $('#trackprogress'),
+
+    events: {
+      "click #progress-link": "show"
+    },
+
+    initialize: function() {
+      $('#progress-link').bind('click', this.show);
+      this.$el.hide();
+
+    },
+
+    show: function(){
+      console.log("in show");
+      window.track.render();
+      $(".visualstate").hide( "fast", function() { 
+        $('#trackprogress').show();
+      });
+      
+    },
+    
+
+    render: function(){
+      this.$el.children().remove();    
+      
+      var allData = window.dailyHabits
+      var habits = window.habits    
+      for (var i = 0;i<habits.length;i++){
+        var progressData = _.map(allData, 
+          function(dataForADay){return dataForADay[i];
+          });
+        var progressSum = _.reduce(progressData, 
+          function(memo, num){return memo + num;
+          });
+        var tm = new TrackModel({name: habits[i], progress:(progressSum*100/7).toFixed(2)});
+        var view = new TrackProgressEntryView({model: tm});
+        this.$el.append(view.render().el)        
+      }
+
+      return this.el;
+    }
+  });
+
+  var trackview = new TrackProgressView;
+  trackview.render();
+  window.track = trackview;
 });
