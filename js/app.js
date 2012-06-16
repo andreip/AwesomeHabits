c4aa7bc18331e2179fe3441f88fb3fd91891ce0a
Dan Serban <dserban01@gmail.com>
2012-06-16 19:46:57 +0300

    Implemented automatic generation of #eachday

--- js/app.js
+++ js/app.js
@@ -14,8 +14,37 @@ for (var di = 0; di < 7; di++) {
     dailyHabits[di][hi]=0;
   }
 }
-$("#notchecked_0_1").click( function(e) { e.preventDefault(); dailyHabits[0][1]=1; $("#notchecked_0_1").hide(); $("#checked_0_1").show(); } );
-$("#checked_0_1").click( function(e) { e.preventDefault(); dailyHabits[0][1]=0; $("#checked_0_1").hide(); $("#notchecked_0_1").show(); } );
-$(".pretend").click( function(e) { e.preventDefault(); currentDay=$(this).attr("id"); $(".visualstate").hide( "fast", function() { $("#eachday").show(); } ); } );
+$(".pretend").click( function(e) { e.preventDefault(); currentDay=parseInt($(this).attr("id")); $(".visualstate").hide( "fast", function() { rewriteEachDayDivFromScratch(); $("#eachday").show(); } ); } );
 $("#gohome").click( function(e) { e.preventDefault(); $(".visualstate").hide( "fast", function() { $("#welcomescreen").show(); } ); } );
+function rewriteEachDayDivFromScratch()
+{
+  $("#eachday").html("");
+  $("#eachday").append("<h2>Well, it's time to check in.</h2>");
+  $("#eachday").append("<h3>Do this at the end of each day.</h3>");
+  $("#eachday").append("<br>");
+  var todaysHabits=dailyHabits[currentDay];
+  for (var hi = 0; hi < todaysHabits.length; hi++) {
+    notchecked_id='notchecked_'+hi;
+    checked_id='checked_'+hi;
+    var txt='';
+    txt+='<div id="'+notchecked_id+'" class="notchecked" style="display: none;">';
+    txt+='<button class="btn btn-mini btn-inverse">☐ '+habits[hi]+'</button>';
+    txt+='</div>';
+    txt+='<div id="'+checked_id+'" class="checked" style="display: none;">';
+    txt+='<button class="btn btn-mini">☑ '+habits[hi]+'</button>';
+    txt+='</div>';
+    txt+='<br>';
+    $("#eachday").append(txt);
+    if (dailyHabits[currentDay][hi] == 0)
+    {
+      $("#"+notchecked_id).show();
+    }
+    else
+    {
+      $("#"+checked_id).show();
+    }
+  }
+  $(".notchecked").click( function(e) { e.preventDefault(); var currentHabit=parseInt($(this).attr("id").replace(/^notchecked_/, "")); dailyHabits[currentDay][currentHabit]=1; $("#notchecked_"+currentHabit).hide(); $("#checked_"+currentHabit).show(); } );
+  $(".checked").click( function(e) { e.preventDefault(); var currentHabit=parseInt($(this).attr("id").replace(/^checked_/, "")); dailyHabits[currentDay][currentHabit]=0; $("#checked_"+currentHabit).hide(); $("#notchecked_"+currentHabit).show(); } );
+}
 
