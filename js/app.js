98bb4fecae7dcbc84face0f7058a92f981a68763
Andrei Petre <p31andrei (at) gmail.com>
2012-06-24 17:19:19 +0300

    Add handlebars templating for #eachday buttons.

--- js/app.js
+++ js/app.js
@@ -1,50 +1,40 @@
-var weekDays=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
-var habits=new Array();
-habits.push("Drink at least 10 glasses of water every day");
-habits.push("Eat a healthy diet");
-habits.push("Spend at least 15 minutes a day learning a new skill");
-habits.push("Be punctual");
-habits.push("Brisk-walk at least 30 minutes a day");
-habits.push("Brush teeth twice a day");
-habits.push("Refrain from watching TV");
-var dailyHabits=new Array();
+var habits = App.Constants['HABITS'];
+var weekDays = App.Constants['WEEKDAYS'];
+var checkBoxes = App.Constants['CHECKBOXES'];
+
+var dailyHabits = new Array();
 for (var di = 0; di < 7; di++) {
-  dailyHabits[di]=new Array();
+  dailyHabits[di] = {};
   for (var hi = 0; hi < habits.length; hi++) {
-    dailyHabits[di][hi]=0;
+    // Create independent memory address objects
+    dailyHabits[di][_.uniqueId('button-')] = Object.create(habits[hi]);
   }
 }
-$(".pretend").click( function(e) { e.preventDefault(); currentDay=parseInt($(this).attr("id")); $(".visualstate").hide( "fast", function() { rewriteEachDayDivFromScratch(); $("#eachday").show(); } ); } );
-$("#gohome").click( function(e) { e.preventDefault(); $(".visualstate").hide( "fast", function() { $("#welcomescreen").show(); } ); } );
+
 function rewriteEachDayDivFromScratch()
 {
-  $("#eachday").html("");
-  $("#eachday").append("<h2>Well, it's time to check in.</h2>");
-  $("#eachday").append("<h3>Do this at the end of each day.</h3>");
-  $("#eachday").append("<br>");
-  var todaysHabits=dailyHabits[currentDay];
-  for (var hi = 0; hi < todaysHabits.length; hi++) {
-    notchecked_id='notchecked_'+hi;
-    checked_id='checked_'+hi;
-    var txt='';
-    txt+='<div id="'+notchecked_id+'" class="notchecked" style="display: none;">';
-    txt+='<button class="btn btn-mini btn-inverse">☐ '+habits[hi]+'</button>';
-    txt+='</div>';
-    txt+='<div id="'+checked_id+'" class="checked" style="display: none;">';
-    txt+='<button class="btn btn-mini">☑ '+habits[hi]+'</button>';
-    txt+='</div>';
-    txt+='<br>';
-    $("#eachday").append(txt);
-    if (dailyHabits[currentDay][hi] == 0)
-    {
-      $("#"+notchecked_id).show();
-    }
-    else
-    {
-      $("#"+checked_id).show();
-    }
+  // Reset the current eachday div
+  el = $("#eachday")
+  el.html("");
+
+  /* Get today's habits and add them to a hashmap
+   * of the form {'habits' : [ ... ]}.
+   */
+  var habits = [];
+  var todaysHabits = dailyHabits[currentDay];
+  for (var key in todaysHabits) {
+    var checked = todaysHabits[key]['checked'];
+    habit = {'checked': checked,
+             'checkbox': checkBoxes[checked],
+             'text': todaysHabits[key]['text'],
+             'button_id': key
+            };
+    habits = habits.concat(habit);
   }
+  params = { 'habits': habits };
+
+  // TODO, get template with requirejs
+
   $(".notchecked").click( function(e) { e.preventDefault(); var currentHabit=parseInt($(this).attr("id").replace(/^notchecked_/, "")); dailyHabits[currentDay][currentHabit]=1; $("#notchecked_"+currentHabit).hide(); $("#checked_"+currentHabit).show(); } );
   $(".checked").click( function(e) { e.preventDefault(); var currentHabit=parseInt($(this).attr("id").replace(/^checked_/, "")); dailyHabits[currentDay][currentHabit]=0; $("#checked_"+currentHabit).hide(); $("#notchecked_"+currentHabit).show(); } );
 }
-
