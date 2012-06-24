5255589fb1b11a447556ee45dc120e7a10441f32
Andrei Petre <p31andrei (at) gmail.com>
2012-06-24 17:12:24 +0300

    Move days, default habits, checkboxes to constants.js.

---
+++ constants.js
@@ -0,0 +1,27 @@
+App.Constants = {
+  HABITS: [
+    {'checked': 0, 'text': 'Drink at least 10 glasses of water every day'},
+    {'checked': 0, 'text': 'Eat a healthy diet'},
+    {'checked': 0, 'text': 'Spend at least 15 minutes a day learning a new skill'},
+    {'checked': 0, 'text': 'Be punctual'},
+    {'checked': 0, 'text': 'Brisk-walk at least 30 minutes a day'},
+    {'checked': 0, 'text': 'Brush teeth twice a day'},
+    {'checked': 0, 'text': 'Refrain from wathcing TV'}
+  ],
+
+  CHECKBOXES: [
+    '☐',  // unchecked
+    '☑'   // checked
+  ],
+
+  WEEKDAYS: [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+  ]
+}
+
